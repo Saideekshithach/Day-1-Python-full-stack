163389fda51450174009ddd71afc32e8aed55b20
Saideekshitha <104785671+Saideekshithach@users.noreply.github.com>
2024-10-18 14:23:03 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,187 @@
+Python 3.11.9 (tags/v3.11.9:de54cf5, Apr  2 2024, 10:12:12) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=========================== RESTART: C:/Users/asus/AppData/Local/Programs/Python/Python311/Variables.py ===========================
+4
+12
+7
+20
+
+=========================== RESTART: C:/Users/asus/AppData/Local/Programs/Python/Python311/Variables.py ===========================
+4
+12
+7
+20
+50
+20
+Deekshi
+India
+
+=========================== RESTART: C:/Users/asus/AppData/Local/Programs/Python/Python311/Variables.py ===========================
+4
+12
+7
+20
+50
+20
+Deekshi
+India
+vja
+hyd
+5a=20
+SyntaxError: invalid decimal literal
+a5=40
+print(a5)
+40
+x123456=100
+print(Z123456)
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    print(Z123456)
+NameError: name 'Z123456' is not defined. Did you mean: 'x123456'?
+print(x123456)
+100
+a=4
+print(a)
+4
+print(5+7)
+12
+b=7
+print(b)
+7
+x=20
+print(x)
+20
+z=50
+print(z)
+50
+_a=30
+print(_a)
+30
+@d=50
+SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?
+abc=20
+print(abc)
+20
+name="Deekshi"
+print(name)
+Deekshi
+country="India"
+print(country)
+India
+place="vja"
+print(place)
+vja
+city="Hyd"
+print(city)
+Hyd
+deeks@hi="deekshi"
+SyntaxError: cannot assign to expression here. Maybe you meant '==' instead of '='?
+first name="deekshi"
+SyntaxError: invalid syntax
+first_name="Saideekshitha"
+last_name="ch"
+print(first_name+last_name)
+Saideekshithach
+print(first_name+" "+last_name)
+Saideekshitha ch
+print(first_name,last_name)
+Saideekshitha ch
+fname="Sai"
+lname="Deekshitha"
+print(fname+lname)
+SaiDeekshitha
+if=50
+SyntaxError: invalid syntax
+Name="Deekshi"
+print(Name)
+Deekshi
+name="Deekshi@21"
+print(name)
+Deekshi@21
+a=2
+b=4
+print(a+b)
+6
+a=5,b=8
+SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?
+a=20,b=40
+SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?
+a=20;b=40
+>>> print(a,b)
+20 40
+>>> print(a+b)
+60
+>>> a,b=4,5
+>>> print(a+b)
+9
+>>> a,b,c,d=1,2,3,4
+>>> print(a,b,c,d)
+1 2 3 4
+>>> fname="SaiDeekshitha";lname="ch"
+>>> print(fname+lname)
+SaiDeekshithach
+>>> print(fname,lname)
+SaiDeekshitha ch
+>>> print(fname+" "+lname)
+SaiDeekshitha ch
+>>> x=7
+>>> x
+7
+>>> z=50
+>>> z
+50
+>>> del z
+>>> z
+Traceback (most recent call last):
+  File "<pyshell#64>", line 1, in <module>
+    z
+NameError: name 'z' is not defined
+>>> del x
+>>> x
+Traceback (most recent call last):
+  File "<pyshell#66>", line 1, in <module>
+    x
+NameError: name 'x' is not defined
+>>> x=15
+>>> del x
+>>> x
+Traceback (most recent call last):
+  File "<pyshell#69>", line 1, in <module>
+    x
+NameError: name 'x' is not defined
+x=20
+x
+20
+del x
+x
+Traceback (most recent call last):
+  File "<pyshell#73>", line 1, in <module>
+    x
+NameError: name 'x' is not defined
+a=5
+b=8
+c=3
+a,b,c
+(5, 8, 3)
+del a,b,c
+a
+Traceback (most recent call last):
+  File "<pyshell#79>", line 1, in <module>
+    a
+NameError: name 'a' is not defined. Did you mean: 'a5'?
+b
+Traceback (most recent call last):
+  File "<pyshell#80>", line 1, in <module>
+    b
+NameError: name 'b' is not defined
+a,b,c=8,9,10
+del a
+b
+9
+del c
+c
+Traceback (most recent call last):
+  File "<pyshell#85>", line 1, in <module>
+    c
+NameError: name 'c' is not defined
